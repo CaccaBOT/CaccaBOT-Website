d9f29c0d603c1a17e1c861aa21d896ec54dd8e5a
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-17 20:53:39 +0100

    Add lenis for smooth scrolling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
+import { onMounted } from 'vue'
+import Lenis from 'lenis'
+
+import 'lenis/dist/lenis.css'
+
+
+onMounted(() => {
+  new Lenis({
+    autoRaf: true
+  })
+})
 </script>
 
 <template>
-  <Navbar />
+  <Navbar/>
   <router-view v-slot="{ Component, route }">
-    <component :is="Component" />
+    <component :is="Component"/>
   </router-view>
 </template>
 
